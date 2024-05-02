a585c180cfb9c4acb37c6f3dc7d23d2ddd0c929a
Ush7 <ush.shilp@gmail.com>
2024-05-02 15:13:08 -0400

    Added weather icon and component

--- src/Weather.js
+++ src/Weather.js
@@ -1,118 +1,68 @@
 import React, { useState } from "react";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
+
 import axios from "axios";
 import "./Weather.css";
 
 export default function Weather(props) {
-  const [weatherData, setWeatherdata] = useState({ ready: false });
+  const [weatherData, setWeatherData] = useState({ ready: false });
   const [city, setCity] = useState(props.defaultCity);
+
   function handleResponse(response) {
-    console.log(response.data);
-    setWeatherdata({
+    setWeatherData({
       ready: true,
-      temperature: Math.round(response.data.main.temp),
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
       humidity: response.data.main.humidity,
-      wind: Math.round(response.data.wind.speed),
+      date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
       tempMax: Math.round(response.data.main.temp_max),
       tempMin: Math.round(response.data.main.temp_min),
-      date: new Date(response.data.dt * 1000),
-      city: response.data.name,
     });
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
   function search() {
     const apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
-  function handleCityChange(event) {
-    setCity(event.target.value);
-  }
+
   if (weatherData.ready) {
     return (
       <div className="Weather">
-        <div className="row heading-section">
-          <div className="col-5">
-            <h2 className="city" id="city">
-              {weatherData.city}
-            </h2>
-            <h7>
-              <div id="date">
-                <FormattedDate date={weatherData.date} />
-              </div>
-              <div id="weatherDescription">{weatherData.description}</div>
-            </h7>
-          </div>
-          <div className="col-7">
-            <form
-              className="d-flex"
-              id="enter-city-form"
-              onSubmit={handleSubmit}
-            >
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
               <input
-                className="form-control me-2"
                 type="search"
-                placeholder="Enter City"
-                id="enter-city-input"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
                 onChange={handleCityChange}
               />
-              <button
-                className="btn btn-primary"
+            </div>
+            <div className="col-3">
+              <input
                 type="submit"
-                id="searchButton"
-              >
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-5">
-            <div className="d-flex todaycolumn1">
-              <div>
-                <img
-                  src={weatherData.iconUrl}
-                  alt={weatherData.description}
-                  id="iconToday"
-                />
-              </div>
-              <div className="current-temp" id="temperature">
-                {weatherData.temperature}
-              </div>
-              <span className="degree" id="celsiusLink">
-                °C{" "}
-              </span>
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-          <div className="col-7">
-            <ul className="todaycolumn2">
-              <li>
-                <div className="todays-high-low">
-                  <span id="highTemp">{weatherData.tempMax}</span>°C |
-                  <span id="lowTemp"> {weatherData.tempMin}</span>
-                  °C
-                </div>
-              </li>
-              <li>
-                <div className="todays-wind">
-                  Wind:
-                  <span id="wind">{weatherData.wind}</span> km/h
-                </div>
-              </li>
-              <li>
-                <div className="todays-humidity">
-                  Humidity:
-                  <span id="humidity">{weatherData.humidity}</span>%
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
+        </form>
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
